b8721968280a43f8e3f5dd2c5f7700551f190a74
Remo2595 <134494175+Remo2595@users.noreply.github.com>
2023-08-04 07:37:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "23097696"))
+API_HASH = getenv("API_HASH", "0e4f1a26757d253329b3bb46fb03ad36")
 
 TOKEN = getenv("TOKEN")
 MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 5938660179))
+OWNER_ID = int(getenv("OWNER_ID", "1410593160")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
 
-LOGGER_ID = getenv("LOGGER_ID", "-1001819078701")
+LOGGER_ID = getenv("LOGGER_ID", "-1001596697160")
